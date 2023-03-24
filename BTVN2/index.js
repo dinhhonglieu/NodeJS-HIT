eb749f9b2ea1b87e48c476006213cc87f47561f0
dinhlieu <dinhhonglieu3@gmail.com>
2023-03-24 13:20:19 +0700

    btvnbuoi2

--- BTVN2/index.js
+++ BTVN2/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const path = require('path')
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'./index.html'))
+  res.sendFile(path.join(__dirname,'./HTML/login.html'))
 })
 app.get('/login', function (req, res){
-  res.sendFile(path.join(__dirname, './login.html'))
+  res.sendFile(path.join(__dirname, './HTML/login.html'))
 })
 app.get('/register', function(req, res){
-  res.sendFile(path.join(__dirname,'./register.html'))
+  res.sendFile(path.join(__dirname,'./HTML/register.html'))
 })
 
 app.listen(3000)
